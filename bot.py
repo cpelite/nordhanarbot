95bd82b52715adf9d8997ec1359941d9a58704ed
unknown <mail@seyffenstein-bajar.de>
2021-03-23 21:59:54 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,45 @@
+## imports
+
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+# loading token
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+# intents festlegen
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+#load extensions, biatch
+bot.load_extension("personennordhanar")
+#bot.load_extension("deltaläuft")
+
+#welcome message
+@bot.event
+async def on_ready():
+    print('Bot wurde als {0.user} eingeloggt.')
+    print("Bot aktiv. Warte auf Befehle.")
+
+@bot.event
+async def botinfo(ctx):
+    embed = discord.Embed(title="Botinfo")
+    embed.add_field(name="Entwickler", value="SvH")
+    embed.add_field(name="Botversion", value="Alpha 1 - Putschvorbereitungen.")
+    embed.add_field(name="Verwendete Programmiersprache", value="Python 3.9")
+    await ctx.send(embed=embed)
+
+# nicht existierender befehl.
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        embed = discord.Embed(title="Sorry!")
+        embed.add_field(name="Es tut mir leid..", value = "...aber dieser Befehl existiert leider noch nicht.")
+        await ctx.send(embed=embed)
+
+
+
+bot.run(TOKEN)
