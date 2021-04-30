8d8029e14ab4a2ba6573609934e8d31572414463
SvH <mail@seyffenstein-bajar.de>
2021-04-30 22:19:51 +0200

    added two gifs

--- fun.py
+++ fun.py
@@ -1,17 +1,23 @@
 from discord.ext import commands
 import discord
 
-
-class fun(commands.Cog):
-    def __init__(self, bot):
+class gifs(commands.Cog):
+    def __init__(self,bot):
         self.bot = bot
 
-        @commands.command()
-        async def wat(self, ctx):
-            embed = discord.Embed(title="Wat?")
-            embed.set_image(url="https://media.tenor.com/images/2c474b0b4404b624d80df839bc688b5f/tenor.gif")
-            await ctx.send(embed=embed)
+    @commands.command()
+    async def wat(self, ctx):
+        embed = discord.Embed(title="Wat?")
+        embed.set_image(url="https://media.tenor.com/images/2c474b0b4404b624d80df839bc688b5f/tenor.gif")
+        await ctx.send(embed=embed)
+        return
 
+    @commands.command()
+    async def confusion(self, ctx):
+        embed = discord.Embed(title="When Confusion kicks in..")
+        embed.set_image(url="https://media.tenor.com/images/afdd108e2e6b46fd825a66e1b92dc87e/tenor.gif")
+        await ctx.send(embed=embed)
+        return
 
 def setup(bot):
-    bot.add_cog(fun(bot))
+    bot.add_cog(gifs(bot))
