3faa809e5bb5b62cfd303236c685f10852b9b089
SvH <mail@seyffenstein-bajar.de>
2021-07-23 21:49:52 +0200

    modified startup script

--- botstart.sh
+++ botstart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Ziehe Änderungen aus dem Github-Repo"
 git pull
-echo "Aktiviere VENV"
-source /venv/bin/activate
+#echo "Aktiviere VENV"
+#source /venv/bin/activate
 echo "Starte Bot"
 nohup python3 bot.py
 echo "Beende Bot."
